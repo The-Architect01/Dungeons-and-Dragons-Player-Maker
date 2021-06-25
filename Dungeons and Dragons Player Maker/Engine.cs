2a6712a326b73e55c95eaf69d248fb46c1438d9d
Neal Birchfield <bircng01@pfw.edu>
2021-06-24 23:46:41 -0400

    Added Serialization/Deserialization

---
+++ Dungeons and Dragons Player Maker/Engine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Dungeons_and_Dragons_Player_Maker {
+    public static class Engine {
+        public static List<PC> Characters {
+            get { return Characters; }
+            set { Characters = value; }
+        }
+    
+        [Obsolete]
+        public static void LoadCharacters() {
+            using MemoryStream ms = new MemoryStream(Convert.FromBase64String(Properties.Settings.Default.Characters)); BinaryFormatter bf = new BinaryFormatter();
+            Characters = (List<PC>)bf.Deserialize(ms);
+        }
+
+        [Obsolete]
+        public static void SaveCharacters() {
+            using MemoryStream ms = new MemoryStream(); BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(ms, Characters);
+            ms.Position = 0;
+            byte[] buffer = new byte[(int)ms.Length];
+            ms.Read(buffer, 0, buffer.Length);
+            Properties.Settings.Default.Characters = Convert.ToBase64String(buffer);
+            Properties.Settings.Default.Save();
+        }
+
+
+    }
+}
--- Dungeons and Dragons Player Maker/PC.cs
+++ Dungeons and Dragons Player Maker/PC.cs
@@ -3,31 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using Microsoft.VisualBasic.Devices;
 
 namespace Dungeons_and_Dragons_Player_Maker {
+    [Serializable]
     public class PC {
-        string Race = "Human";
-        string Subrace = "";
-        public string getRace() { return Subrace == "" ? Race : Race + "-" + Subrace; }
-        string Class = "Cleric";
-        string Subclass = "War";
-        public string getClass() { return Class + "-" + Subclass; }
-        string Background = "Acolyte";
-        public string getBackground() { return Background; }
+        public string Name { get; private set; }
 
-        string[] Inventory;
-        public string[] getInventory() { return Inventory ; }
-        string[] Personality;
-        public string[] getPersonality() { return Personality; }
+        public string Race { get; private set; }
+        public string Class { get; private set; }
+        public string Background { get; private set; }
 
-        int XP;
-        int Level;
+        public string[] Inventory { get; private set; }
+        public string[] Personality { get; private set; }
+        public List<string> Skills { get; private set; }
+        public string Alignment { get; private set; }
+        public int[] Stats { get; private set; }
 
-        public PC(string Race,string subrace,string Class,string Subclass,string Background,string[] Personality,int XP,params string[] Inventory) {
+        public int XP { get; private set; }
+        public int Level { get; private set; }
+        public string creator { get; private set; }
+
+        public PC(string Race,string Class,string Background,string[] Personality,int XP,params string[] Inventory) {
             this.Race = Race;
-            this.Subrace = subrace;
             this.Class = Class;
-            this.Subclass = Subclass;
             this.Background = Background;
             this.Personality = Personality;
             this.Inventory = Inventory;
@@ -60,5 +62,21 @@ namespace Dungeons_and_Dragons_Player_Maker {
 
         public void gainXP(int XP) { if (XP <= 355000) { this.XP += XP; } else { showError(); } }      
         public void showError() { System.Windows.Forms.MessageBox.Show("You can't gain anymore XP!"); }
+
+        [Obsolete]
+        public void save() {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + Name + " " +Class + ".hro";
+            using (FileStream fs = File.Create(path)) {
+                BinaryFormatter bin = new BinaryFormatter();
+                bin.Serialize(fs, this);
+            }
+            MessageBox.Show("The data was saved to your desktop.");
+        }
+
+        [Obsolete]
+        public static PC openCharacter(string location) {
+            using Stream stream = File.Open(location, FileMode.Open); BinaryFormatter bin = new BinaryFormatter();
+            return (PC)bin.Deserialize(stream);
+        }
     }
 }
