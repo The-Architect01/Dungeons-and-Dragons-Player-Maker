1b957f199eda365452e0767fad2e87d2434353c4
The-Architect01 <84486562+The-Architect01@users.noreply.github.com>
2021-08-05 21:29:01 -0400

    Started the Customizaation Tab

--- Dungeons and Dragons Player Maker/Player Maker/CustomizationTab.cs
+++ Dungeons and Dragons Player Maker/Player Maker/CustomizationTab.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Dungeons_and_Dragons_Player_Maker.Player_Maker {
@@ -11,8 +12,25 @@ namespace Dungeons_and_Dragons_Player_Maker.Player_Maker {
         bool _ready = false;
         bool informationFilled { get { return _ready; } set { _ready = value; if (value) { OnReady.Invoke(this, EventArgs.Empty); } } }
 
-        public CustomizationTab() {
+        public CustomizationTab(PC Player) {
+            PC = Player;
+            Text = "Customize";
+            BackColor = Color.White;
             InitializeComponent();
+            Pages.TabPages.Add(ClassData); Pages.TabPages.Add(StatsData);
         }
+
+        #region Controls
+        TabControl Pages = new() {
+            Size = new Size(492, 540),
+            Location = new Point(6, 6),
+        };
+        TabPage ClassData = new() {
+            Text = "Class Customization Options",
+        };
+        TabPage StatsData = new() {
+            Text = "Abilities and Stats",
+        };
+        #endregion
     }
 }
